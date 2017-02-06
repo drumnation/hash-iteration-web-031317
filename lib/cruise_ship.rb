715f41a46ae104660cd60dd40e5152fb29905575
David Mieloch <davidmieloch@gmail.com>
2017-02-06 23:33:12 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@ passengers = {
 
 def select_winner(passengers)
   # must be staying in Suite A and their name must start with the letter "A
-  passengers.select { |key, value| return value if :suite_a && value.start_with?("a", "A") }
+  passengers.select { |suite, name| return name if :suite_a && name.start_with?("a", "A") }
   # QUESTION: What enumerator can I use for this return the
   # hash value without the case statement and explicit return?
 end
